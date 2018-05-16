000205be59b60e00aec0b1869c35cf0da3b18ee4
Bill Wang <bill.wang@team.telstra.com>
2018-05-16 21:53:42 +1000

    Adjust with environment variables

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # aws-iot-slack
 Connect an AWS IoT Button to a Slack Channel
+
+This lambda function is used by [Tutorial — Amazon IoT Slack Button](https://blog.glassboard.com/tutorial-aws-iot-lunchtime-slack-button-295d78f4a9a6). I adjust and use environment variables more than hard coding them.
+
+```
+const encrypted = process.env['slack_token'];
+const public_channel = process.env['public_channel'];
+const private_channel = process.env['private_channel'];
+const button_serial = process.env['button_serial'];
+const bot_name = process.env['bot_name'];
+```
--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const Slackbot = require('slackbots');
 const AWS = require('aws-sdk');
 
 const encrypted = process.env['slack_token'];
+const public_channel = process.env['public_channel'];
+const private_channel = process.env['private_channel'];
+const button_serial = process.env['button_serial'];
+const bot_name = process.env['bot_name'];
 let decrypted;
 
 //List the name of the group you want the notficiations posted to
-var channel = 'general';
+//var channel = 'general';
 
 //For each button list the name of the user and their IoT button serial number found on the back of the device
 var buttonSerial = {
-    Carl: 'G030XXXXXXXXXXXX',
-    Wendy: 'G030XXXXXXXXXXXX'
+    Carl: button_serial
 };
 
 //I highly suggest using encrypted env variables so you don't expose your slack api token
@@ -22,7 +25,9 @@ exports.handler = (event, context, callback) => {
         // Decrypt code should run once and variables stored outside of the function
         // handler so that these are decrypted once per container
         const kms = new AWS.KMS();
-        kms.decrypt({ CiphertextBlob: new Buffer(encrypted, 'base64') }, (err, data) => {
+        kms.decrypt({
+            CiphertextBlob: new Buffer(encrypted, 'base64')
+        }, (err, data) => {
             if (err) {
                 console.log('Decrypt error:', err);
                 return callback(err);
@@ -35,8 +40,8 @@ exports.handler = (event, context, callback) => {
 
 function processEvent(event, context, callback) {
     var edwin = new Slackbot({
-        token: decrypted,  //your decrypted slack token will be used here
-        name: 'YOUR_BOT_NAME'
+        token: decrypted, //your decrypted slack token will be used here
+        name: bot_name
     });
     console.log('Received event:', event);
     const payload = JSON.stringify(event);
@@ -44,7 +49,7 @@ function processEvent(event, context, callback) {
         case buttonSerial.Carl:
             switch (event.clickType) {
                 case "SINGLE":
-                    edwin.postMessageToChannel(channel, "", {
+                    edwin.postMessageToChannel(public_channel, "", {
                         as_user: 'true',
                         attachments: [{
                             title: "Carl is ready for lunch!",
@@ -56,9 +61,9 @@ function processEvent(event, context, callback) {
                     }).then(function(data) {
                         context.done(null, data);
                     });
-                break;
+                    break;
                 case "DOUBLE":
-                    edwin.postMessageToChannel(channel, "", {
+                    edwin.postMessageToGroup(private_channel, "", {
                         as_user: 'true',
                         attachments: [{
                             title: "Carl packed his lunch today :(",
@@ -70,40 +75,8 @@ function processEvent(event, context, callback) {
                     }).then(function(data) {
                         context.done(null, data);
                     });
-                break;
+                    break;
             }
-        break;
-        case buttonSerial.Wendy:
-            switch (event.clickType) {
-                case "SINGLE":
-                    edwin.postMessageToChannel(channel, "", {
-                        as_user: 'true',
-                        attachments: [{
-                            title: "Wendy is ready for lunch!",
-                            fallback: "Lunchtime Alert | IoT Button",
-                            text: "Lunchtime Alert | IoT Button",
-                            image_url: "http://i.giphy.com/11dgYWwHnEFomQ.gif",
-                            color: "#01BAEF"
-                        }]
-                    }).then(function(data) {
-                        context.done(null, data);
-                    });
-                break;
-                case "DOUBLE":
-                    edwin.postMessageToChannel(channel, "", {
-                        as_user: 'true',
-                        attachments: [{
-                            title: "Wendy packed her lunch today :(",
-                            fallback: "Lunchtime Alert | IoT Button",
-                            text: "Lunchtime Alert | IoT Button",
-                            image_url: "http://i.giphy.com/ZOPQqUVKtUOI0.gif",
-                            color: "#01BAEF"
-                        }]
-                    }).then(function(data) {
-                        context.done(null, data);
-                    });
-                break;
-            }
-        break;
+            break;
     }
 }
